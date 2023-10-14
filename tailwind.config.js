b69c3be7b92a93704d77c5321fd01a61a7a8cd58
charlenefialho <fialhocharlene@gmail.com>
2023-10-14 17:23:46 -0300

    feat: added html structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,22 @@ module.exports = {
     extend: {
       colors: {
         white: "#ffffff",
+        black: "#000000",
         "blue-800": "#232740",
       },
       screens: {
         mlarge: "425px",
-        lplarge:"1440px"
+        lplarge: "1440px",
       },
-      minWidth:{
-        '1/2': '50%',
-        '9/12': '75%',
-        '10/12':'83.333333%',
-        '11/12':'91.666667%',
-      }
+      minWidth: {
+        "1/2": "50%",
+        "9/12": "75%",
+        "10/12": "83.333333%",
+        "11/12": "91.666667%",
+      },
+      gridTemplateColumns: {
+        "2": "repeat(2, minmax(50%, 50%))",
+      },
     },
   },
   plugins: [],
