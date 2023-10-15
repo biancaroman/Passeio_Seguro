91f6f652c9c994fab837a56a36c3ee6691815477
charlenefialho <fialhocharlene@gmail.com>
2023-10-15 15:00:46 -0300

    refactor: order imports

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,7 +1,9 @@
 'use client'
 
-import animationData from "@public/lottie/error-404.json";
 import Lottie from "lottie-react";
+
+import animationData from "@public/lottie/error-404.json";
+
 export default function Custom404() {
   return (
     <div className="flex flex-col w-full justify-center items-center">
