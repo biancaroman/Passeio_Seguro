3f04576c4f9d5cd3636866dbe5fa3206beb0639b
biancaroman <biancaroman1425@gmail.com>
2023-11-03 18:39:12 -0300

    feat: added home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import { Header } from "@/app/components/Header";
 import Footer from "./components/Footer";
+import Inicio from "./inicio";
 
 export default function Home() {
   return (
     <>
       <Header isAuthenticated={false} />
-      <h1>Hello world</h1>
+      <Inicio/>
       <Footer/>
     </>
   );
