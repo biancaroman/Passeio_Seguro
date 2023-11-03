385f897720bd4603ed3c0c7261a7c118965537af
biancaroman <biancaroman1425@gmail.com>
2023-11-03 14:23:33 -0300

    feat:add-footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import { Header } from "@/app/components/Header";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <>
       <Header isAuthenticated={false} />
       <h1>Hello world</h1>
+      <Footer/>
     </>
   );
 }
